f9738384ea1382dcb4748d6946c844b2f89f66eb
victor <victor.gachure@student.moringaschool>
2024-06-02 10:16:03 +0300

    done lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
 # your code goes here!
+# lib/anagram.py
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, possible_anagrams):
+        # Sort the letters of the word we are checking against
+        sorted_word = sorted(self.word)
+        
+        # List to store anagrams
+        anagrams = []
+        
+        for candidate in possible_anagrams:
+            # Check if the sorted version of the candidate matches the sorted_word
+            if sorted(candidate) == sorted_word:
+                anagrams.append(candidate)
+        
+        return anagrams
